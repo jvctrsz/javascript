00fbc24f40b98e7bf9beb381f2f0d80c9c3c45e7
João Matias <joaosteam26@gmail.com>
2022-11-29 21:06:34 -0300

    att

--- exercicios/curso js/script.js
+++ exercicios/curso js/script.js
@@ -1,11 +1,21 @@
 // -------Variaves-------
 
 
-let nome = 'Henrique';
+let nome = 'Henrique';  //string literal (conjunto de letras)
 console.log(nome);
 
-let altura = '1.80m';
+let altura = '1.80m';   //number literal (conjunto de numeros)
 console.log(altura);
 
-let idade = 18;
+let idade = 18;         //number literal (conjunto de numeros)
 console.log(idade);
+
+let estaAprovado = true; //boolean (Regras)
+
+let sobrenome ;         //undefined 
+
+let corSelecionada = null; //Redefinir um valor
+
+
+
+ 
