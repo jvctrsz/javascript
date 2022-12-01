c6fae2cd26c3fed45041d4e2415ccb2ab2ceea5c
João Matias <joaosteam26@gmail.com>
2022-11-30 22:08:14 -0300

    loop

--- exercicios/aula3/script.js
+++ exercicios/aula3/script.js
@@ -4,3 +4,70 @@
 // Se estiver entre 12:00 até 18:00 : Boa tarde!
 // Caso contrário : Boa noite!
 
+let hora = 6;
+
+if (hora >= 6 && hora < 12) {
+    console.log('Bom dia!')
+}
+
+else if (hora > 12 && hora <= 18) {
+    console.log('Boa tarde!')
+}
+
+else {
+    console.log('Boa noite!')
+}
+
+//SWitch .. Case
+
+
+let permissao; //comum, gerente, diretor
+
+permissao = '';
+
+switch (permissao) {
+    case 'comum':
+    console.log('usuário comum');
+    break
+
+    case 'gerente':
+    console.log('usuário gerente');
+    break
+
+    case 'diretor':
+    console.log('usuário diretor');
+    break
+
+    default:
+    console.log('usuário não reconhecido');
+}
+
+//Laço For / Loop for
+
+// 1.For
+// 2.While
+// 3.Do..While
+// 4.For..In
+// 5.For..of
+
+for(let i = 1; i <= 5; i++) {
+    console.log('Estou aprendendo!', i)
+}
+
+for(let i = 1; i <= 5; i++) {
+    if(i % 2 !== 0) { // ESTUDAR MODULUS
+        console.log(i);
+    }
+}
+
+
+//While loop
+
+let i =5;
+
+while (i <= 5) {
+    if(i % 2 !== 0) { // ESTUDAR MODULUS
+        console.log(i);
+    }
+    i++;
+}
