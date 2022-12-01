93f4339e5c632ae2599f4fc98406010bd338e10d
João Matias <joaosteam26@gmail.com>
2022-11-30 22:57:59 -0300

    arquivos

---
+++ desafios/mini-projeto-2/script.js
@@ -0,0 +1,17 @@
+// let valorMaior = maiorNumero(9,12);
+
+// function maiorNumero(numero1, numero2){
+//     if (numero1 > numero2)
+//         return numero1;
+//         return numero2;
+// }
+
+
+
+
+let valorMaior = maiorNumero(7,11);
+console.log(valorMaior)
+
+function maiorNumero(numero1, numero2){
+    return numero1 > numero2 ? numero1: numero2 ;
+}
