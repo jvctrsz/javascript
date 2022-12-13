dad6f96443c2da754e25d122246c23632e8eadbf
João Matias <joaosteam26@gmail.com>
2022-12-12 22:38:19 -0300

    fim dia

---
+++ desafios/mini-projeto-13/script.js
@@ -0,0 +1,11 @@
+let postagem = {
+    titulo: 'One Piece',
+    autor: 'Eichiiro Oda',
+    vizualizaçoes: '500 mil',
+    comentarios: [
+        {autor: 'a', mensagem: 'b'}
+    ],
+    estaAoVivo: true
+}
+
+console.log(postagem)
