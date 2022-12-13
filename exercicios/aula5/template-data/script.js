b00c92b8f0734544e6bcd9c13df3cd90ce9c17b6
João Matias <joaosteam26@gmail.com>
2022-12-12 21:51:32 -0300

    att

---
+++ exercicios/aula5/template-data/script.js
@@ -0,0 +1,11 @@
+// TEMPLATE LITERAL
+
+// alert(`Eu sou 
+// um alerta para testar
+
+
+//DATE
+
+const data1 = new Date();
+const data2 = new Date('December 12 2022 20:47');
+const data3 = new Date(2022,11,12,20,48);
