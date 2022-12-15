17f05d6fd2eb776af0483a6429747b0bf7deb49a
João Matias <joaosteam26@gmail.com>
2022-12-14 21:17:10 -0300

    add elements

---
+++ exercicios/aula6-ARRAYS/add-elements/script.js
@@ -0,0 +1,14 @@
+//ARRAYS
+
+//Como adicionar um elemento dentro de um array usando os metodos
+//nameArray.unshift(); adiciona um elemento dentro da array
+//nameArray.splice(); adiciona um elemento entre um indice desejado da array
+//nameArray.push(); adiciona um elemento no final da array
+
+const carro = ['andar','virar','manobrar'];
+
+carro.unshift('ligar');
+
+carro.splice(1,0,'acelerar');
+
+carro.push('estacionar')
